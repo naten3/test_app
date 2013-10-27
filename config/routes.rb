4afdbf66a66f0d106ca617ab02b6fe7b4f04731e
nate <nnelles1@gmail.com>
2013-10-26 21:48:23 -0500

    Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TestApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new', via: :get
+
   match '/contact', :to => 'pages#contact', via: :get
   match '/about', :to => 'pages#about', via: :get
   match '/help', :to => 'pages#help', via: :get
